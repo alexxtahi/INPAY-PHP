fd1b7527466f91b5b28f0192392f990a05787d7f
TAMO Ngouobou Calvine Chantal <tamochantal5@gmail.com>
2022-04-23 12:28:43 +0000

    Add Seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            `id`=> Str::random(12),
+            `code_prod`=> Str::random(12),
+            `designation`=> Str::random(12),
+            `description`=> Str::random(12),
+            `qte_prod`=> Str::random(12),
+            `img_prod`=> Str::random(12),
+            `prix_prod`=> Str::random(12),
+            `ancien_prix`=> Str::random(12),
+            `en_promo`=> Str::random(12),
+            `id_cat`=> Str::random(12),
+            `id_sous_cat`=> Str::random(12),
+            `deleted_at`=> Str::random(12),
+            `updated_by`=> Str::random(12),
+            `deleted_by`=> Str::random(12),
+            `created_by`=> Str::random(12),
+            `created_at`=> Str::random(12),
+            `updated_at`
+            
+        ]);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,54 +84,49 @@
     </header>
     <!-- Header Section End -->
 
-    <!-- Banner Section Begin -->
-   
-    <section class="banner spad">
-        <div class="container">
-            <div class="row">
-
-
-            @foreach ($produits as $produits)
-                <div class="col-lg-7 offset-lg-4">
-                    <div class="banner__item">
-                        <div class="banner__item__pic">
-                        <title><?= $produits->nom ?></title><img src="<?= $produits->image ?>" alt="">
-                        </div>
-                        <div class="banner__item__text">
-                            <h2>Clothing Collections 2030</h2>
-                            <a href="#">Shop now</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5">
-                    <div class="banner__item banner__item--middle">
-                        <div class="banner__item__pic">
-                            <img src="{{ asset('template/img/banner/banner-2.jpg') }}" alt="">
-                        </div>
-                        <div class="banner__item__text">
-                            <h2>Accessories</h2>
-                            <a href="#">Shop now</a>
+    <!-- Hero Section Begin -->
+    <section class="hero">
+            <div class="hero__slider owl-carousel">
+                <div class="hero__items set-bg"
+                    data-setbg="{{ asset('template/img/hero/hero-1.jpg') }}">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-5 col-lg-7 col-md-8">
+                                <div class="hero__text">
+                                    <h6>Summer Collection</h6>
+                                    <h2>Fall - Winter Collections 2030</h2>
+                                    <p>A specialist label creating luxury
+                                        essentials. Ethically crafted with an
+                                        unwavering
+                                        commitment to exceptional quality.</p>
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-7">
-                    <div class="banner__item banner__item--last">
-                        <div class="banner__item__pic">
-                            <img src="{{ asset('template/img/banner/banner-3.jpg') }}" alt="">
-                        </div>
-                        <div class="banner__item__text">
-                            <h2>Shoes Spring 2030</h2>
-                            <a href="#">Shop now</a>
+                <div class="hero__items set-bg"
+                    data-setbg="{{ asset('template/img/hero/hero-2.jpg') }}">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-5 col-lg-7 col-md-8">
+                                <div class="hero__text">
+                                    <h6>Summer Collection</h6>
+                                    <h2>Fall - Winter Collections 2030</h2>
+                                    <p>A specialist label creating luxury
+                                        essentials. Ethically crafted with an
+                                        unwavering
+                                        commitment to exceptional quality.</p>
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-
-                @endforeach
             </div>
-        </div>
-    </section>
-    <!-- Banner Section End -->
+        </section>
+        <!-- Hero Section End -->
+
 
     <!-- Product Section Begin -->
     <section class="product spad">
@@ -146,23 +141,17 @@
                 </div>
             </div>
             <div class="row product__filter">
+
+            @foreach ($produits as $produits)
                 <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix
                         new-arrivals">
                     <div class="product__item">
                         <div class="product__item__pic set-bg"
                             data-setbg="{{ asset('template/img/product/product-1.jpg') }}">
                             <span class="label">New</span>
-                            <ul class="product__hover">
-                                <li><a href="#"><img src="{{ asset('template/img/icon/heart.png') }}" alt=""></a>
-                                </li>
-                                <li><a href="#"><img src="{{ asset('template/img/icon/compare.png') }}" alt="">
-                                        <span>Compare</span></a></li>
-                                <li><a href="#"><img src="{{ asset('template/img/icon/search.png') }}" alt=""></a>
-                                </li>
-                            </ul>
                         </div>
                         <div class="product__item__text">
-                            <h6>Piqué Biker Jacket</h6>
+                            <h6><?= $produits->designation ?></h6>
                             <a href="#" class="add-cart">+ Add To Cart</a>
                             <div class="rating">
                                 <i class="fa fa-star-o"></i>
@@ -171,7 +160,7 @@
                                 <i class="fa fa-star-o"></i>
                                 <i class="fa fa-star-o"></i>
                             </div>
-                            <h5>$67.24</h5>
+                            <h5><?= $produits->prix_prod ?></h5>
                             <div class="product__color__select">
                                 <label for="pc-1">
                                     <input type="radio" id="pc-1">
@@ -186,7 +175,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix
+               <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix
                         hot-sales">
                     <div class="product__item">
                         <div class="product__item__pic set-bg"
@@ -461,99 +450,13 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>-->
+            @endforeach; 
         </div>
     </section>
     <!-- Product Section End -->
 
 
-    <!-- Instagram Section Begin -->
-    <section class="instagram spad">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8">
-                    <div class="instagram__pic">
-                        <div class="instagram__pic__item set-bg"
-                            data-setbg="{{ asset('template/img/instagram/instagram-1.jpg') }}"></div>
-                        <div class="instagram__pic__item set-bg"
-                            data-setbg="{{ asset('template/img/instagram/instagram-2.jpg') }}"></div>
-                        <div class="instagram__pic__item set-bg"
-                            data-setbg="{{ asset('template/img/instagram/instagram-3.jpg') }}"></div>
-                        <div class="instagram__pic__item set-bg"
-                            data-setbg="{{ asset('template/img/instagram/instagram-4.jpg') }}"></div>
-                        <div class="instagram__pic__item set-bg"
-                            data-setbg="{{ asset('template/img/instagram/instagram-5.jpg') }}"></div>
-                        <div class="instagram__pic__item set-bg"
-                            data-setbg="{{ asset('template/img/instagram/instagram-6.jpg') }}"></div>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="instagram__text">
-                        <h2>Instagram</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur
-                            adipiscing elit, sed do eiusmod tempor
-                            incididunt ut
-                            labore et dolore magna aliqua.</p>
-                        <h3>#Male_Fashion</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Instagram Section End -->
-
-    <!-- Latest Blog Section Begin -->
-    <section class="latest spad">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section-title">
-                        <span>Latest News</span>
-                        <h2>Fashion New Trends</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="blog__item">
-                        <div class="blog__item__pic set-bg"
-                            data-setbg="{{ asset('template/img/blog/blog-1.jpg') }}"></div>
-                        <div class="blog__item__text">
-                            <span><img src="{{ asset('template/img/icon/calendar.png') }}" alt="">
-                                16 February 2020</span>
-                            <h5>What Curling Irons Are The Best Ones</h5>
-                            <a href="#">Read More</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="blog__item">
-                        <div class="blog__item__pic set-bg"
-                            data-setbg="{{ asset('template/img/blog/blog-2.jpg') }}"></div>
-                        <div class="blog__item__text">
-                            <span><img src="{{ asset('template/img/icon/calendar.png') }}" alt="">
-                                21 February 2020</span>
-                            <h5>Eternity Bands Do Last Forever</h5>
-                            <a href="#">Read More</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="blog__item">
-                        <div class="blog__item__pic set-bg"
-                            data-setbg="{{ asset('template/img/blog/blog-3.jpg') }}"></div>
-                        <div class="blog__item__text">
-                            <span><img src="{{ asset('template/img/icon/calendar.png') }}" alt="">
-                                28 February 2020</span>
-                            <h5>The Health Benefits Of Sunglasses</h5>
-                            <a href="#">Read More</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Latest Blog Section End -->
 
     <!-- Footer Section Begin -->
     @include('include\footer')
