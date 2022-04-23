6cddd7911ee15b8a9a2b210dc085fa842dd22201
TAMO Ngouobou Calvine Chantal <tamochantal5@gmail.com>
2022-04-23 12:06:29 +0000

    Navbar modif

---
+++ resources/views/include/js.blade.php
@@ -0,0 +1,11 @@
+
+<script src="{{ asset('template/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('template/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.countdown.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.slicknav.js') }}"></script>
+    <script src="{{ asset('template/js/mixitup.min.js') }}"></script>
+    <script src="{{ asset('template/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('template/js/main.js') }}"></script>
