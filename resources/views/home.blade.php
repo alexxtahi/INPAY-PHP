6508cd2143624c122007942fb4afc010d6407eb3
AlexTAHI <62557152+AlexTAHI@users.noreply.github.com>
2022-04-23 12:42:20 +0000

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,85 +1,14 @@
 <head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Male_Fashion Template">
-    <meta name="keywords" content="Male_Fashion, unica, creative, html">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Shop Hackathon- 2022</title>
+    @include('include.meta')
 
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
-        rel="stylesheet">
 
     <!-- Css Styles -->
     @include('include.css')
 </head>
 
 <body>
-    <!-- Page Preloder -->
-    <div id="preloder">
-        <div class="loader"></div>
-    </div>
-
-    <!-- Offcanvas Menu Begin -->
-    <div class="offcanvas-menu-overlay"></div>
-    <div class="offcanvas-menu-wrapper">
-        <div class="offcanvas__option">
-            <div class="offcanvas__links">
-                <a href="#">Sign in</a>
-                <a href="#">FAQs</a>
-            </div>
-            <div class="offcanvas__top__hover">
-                <span>Usd <i class="arrow_carrot-down"></i></span>
-                <ul>
-                    <li>USD</li>
-                    <li>EUR</li>
-                    <li>USD</li>
-                </ul>
-            </div>
-        </div>
-        <div class="offcanvas__nav__option">
-            <a href="#" class="search-switch"><img src="{{ asset('template/img/icon/search.png') }}" alt=""></a>
-            <a href="#"><img src="{{ asset('template/img/icon/heart.png') }}" alt=""></a>
-            <a href="#"><img src="{{ asset('template/img/icon/cart.png') }}" alt=""> <span>0</span></a>
-            <div class="price">$0.00</div>
-        </div>
-        <div id="mobile-menu-wrap"></div>
-        <div class="offcanvas__text">
-            <p>Free shipping, 30-day return or refund guarantee.</p>
-        </div>
-    </div>
-    <!-- Offcanvas Menu End -->
-
-    <!-- Header Section Begin -->
-    <header class="header">
-
-        <div class="container">
-            <div class="row">
-
-                <div class="col-lg-6 col-md-6">
-                    <nav class="header__menu mobile-menu">
-                        <ul>
-                            <li class="active"><a href="./index.html">Home</a></li>
-                            <li><a href="./shop.html">Apropos</a></li>
-
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-lg-3 col-md-3">
-                    <div class="header__nav__option">
-                        <a href="#" class="search-switch"><img src="{{ asset('template/img/icon/search.png') }}"
-                                alt=""></a>
-
-                        <a href="#"><img src="{{ asset('template/img/icon/cart.png') }}" alt="">
-                            <span>0</span></a>
-
-                    </div>
-                </div>
-            </div>
-            <div class="canvas__open"><i class="fa fa-bars"></i></div>
-        </div>
-    </header>
-    <!-- Header Section End -->
+    <!-- Header Section -->
+    @include('include.header')
 
     <!-- Hero Section Begin -->
     <section class="hero">
@@ -128,7 +57,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <ul class="filter__controls">
+                    <ul class="filter__controls" style="margin-top: 45px">
                         <li class="active" data-filter="*">Best Sellers</li>
                         <li data-filter=".new-arrivals">New Arrivals</li>
                         <li data-filter=".hot-sales">Hot Sales</li>
@@ -137,7 +66,7 @@
             </div>
             <div class="row product__filter">
 
-                @foreach ($produits as $produits)
+                @foreach ($produits as $produit)
                     <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix
                         new-arrivals">
                         <div class="product__item">
@@ -146,7 +75,7 @@
                                 <span class="label">New</span>
                             </div>
                             <div class="product__item__text">
-                                <h6><?= $produits->designation ?></h6>
+                                <h6><?= $produit->designation ?></h6>
                                 <a href="#" class="add-cart">+ Add To Cart</a>
                                 <div class="rating">
                                     <i class="fa fa-star-o"></i>
@@ -155,7 +84,7 @@
                                     <i class="fa fa-star-o"></i>
                                     <i class="fa fa-star-o"></i>
                                 </div>
-                                <h5><?= $produits->prix_prod ?></h5>
+                                <h5><?= $produit->prix_prod ?></h5>
                                 <div class="product__color__select">
                                     <label for="pc-1">
                                         <input type="radio" id="pc-1">
@@ -454,8 +383,7 @@
 
 
     <!-- Footer Section Begin -->
-    @include('include\footer')
-    <!-- Footer Section End -->
+    @include('include.footer')
 
     <!-- Search Begin -->
     <div class="search-model">
